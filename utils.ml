1d33be3ac4f4429602383a335bcab52b7783ad9d
Erkki Seppälä <flux@modeemi.fi>
2012-05-17 20:16:03 +0300

    Utils.sumsq, .stddev: added

--- utils.ml
+++ utils.ml
@@ -1,5 +1,7 @@
 let sum xs = Array.fold_left (+.) 0.0 xs 
 
+let sumsq xs = Array.fold_left (fun a b -> a +. b *. b) 0.0 xs 
+
 let sum_list xs = List.fold_left (+.) 0.0 xs 
 
 let minimum xs = Array.fold_left min infinity xs
@@ -7,6 +9,10 @@ let minimum xs = Array.fold_left min infinity xs
 let maximum xs = Array.fold_left max neg_infinity xs
 
 let average xs = sum xs /. float (Array.length xs) 
+
+let stddev xs =
+  let avg = average xs in
+    sqrt (sumsq (BatArray.map (fun a -> a -. avg) xs) /. float (Array.length xs - 1))
 
 let average_list xs = sum_list xs /. float (List.length xs) 
 
