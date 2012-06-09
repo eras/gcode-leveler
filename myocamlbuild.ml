3cfe8f71a0441a358fbecd60049b90b0f40094f8
Erkki Seppälä <flux@modeemi.fi>
2012-06-09 14:55:41 +0300

    myocamlbuild: don't use -custom with native binary

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -4,7 +4,8 @@ open Command
 let _ = dispatch begin function
   | After_rules ->
       flag ["c"; "compile"] (S[A"-ccopt"; A"-g"]);
-      flag ["ocaml"; "link"; "use_v4l2"] (S[A"-custom"; A"-ccopt"; A"-g -ljpeg -Wall -W -Wno-unused-parameter"]);
+      flag ["ocaml"; "link"; "use_v4l2"; "native"] (S[A"-ccopt"; A"-g -ljpeg -Wall -W -Wno-unused-parameter"]);
+      flag ["ocaml"; "link"; "use_v4l2"; "byte"] (S[A"-custom"]);
       dep ["link"; "ocaml"; "use_v4l2"] ["v4l2_stubs.o"]
   | _ -> ()
 end
